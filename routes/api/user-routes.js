eb522651de3a974c8a4eca005ff294bb7f8d1fbe
spectocr <highstakes22@gmail.com>
2021-07-17 11:37:30 -0400

    yassss finishing 13.2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,31 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+    // add comment syntax in front of this line in the .then()
+    //res.json({ user: dbUserData });
+
+    //verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
