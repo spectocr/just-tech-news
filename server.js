9635a0c41bc38337da15d4fdcab700a47b70a0ea
spectocr <highstakes22@gmail.com>
2021-07-24 20:47:57 -0400

    finishing 14.4

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
@@ -20,8 +22,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
